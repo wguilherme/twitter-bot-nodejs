b5463c4373dc4e7e6d94417bb63900040a5a007d
W. Guilherme <ravok123@gmail.com>
2021-06-04 15:53:43 -0300

    [bot-retweet] add retweet feature

--- bot-retweet/src/app.js
+++ bot-retweet/src/app.js
@@ -1,6 +1,5 @@
 const config = require('./config')
 const twit = require('twit')
-
 const Tweet = new twit(config)
 
 function retweet(searchText) {
@@ -10,22 +9,34 @@ function retweet(searchText) {
       count: 25
    }
 
-
    Tweet.get('search/tweets', params, (err, data, res) => {
-
 
       let tweets = data.statuses
       if (!err) {
+
+         let tweetIDList = []
+         for (let tweet of tweets) {
+            tweetIDList.push(tweet.id_str)
+         }
+
+         for (let tweetID of tweetIDList) {
+            Tweet.post('statuses/retweet/:id', { id: tweetID }, function (err, data, res) {
+               if (!err) {
+                  console.log("Retweeted ID - " + tweetID)
+               }
+               else {
+                  console.log("Error in ", tweetID)
+                  console.log("Error ", err)
+               }
+            })
+         }
+
          console.log(data)
       }
       else {
          return console.log('Error while searching' + err)
-
       }
-
    })
-
 }
 
-// run every 1min
 setInterval(() => { reweet('#Javascript OR #100daysofcode') }, 60000)
